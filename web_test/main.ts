dd297097afe4c682773bde4cdd02ea29d4fa9423
alpertunga-bile <kingdom19online@hotmail.com>
2024-12-26 23:54:06 +0300

    new funcs are added

--- web_test/main.ts
+++ web_test/main.ts
@@ -1,7 +1,5 @@
 import { greet } from "../pkg/cacher_wasm.js";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const temp = greet();
-  console.log(temp);
+  console.log(greet());
 }
