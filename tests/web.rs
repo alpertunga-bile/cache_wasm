1e0a42da5cbe56e63dcec7bc11502c841f5d6529
alpertunga-bile <kingdom19online@hotmail.com>
2025-01-07 20:57:16 +0300

    hello world test is added

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,22 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use std::assert_eq;
+
+use cacher_wasm::{
+    get_compressed_cacher_info, get_decompressed_data, CacherOptions, CacherReturnInfo,
+};
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn hello_world_test() {
+    let options = CacherOptions::new();
+    let data = String::from("Hello World");
+
+    let compressed_info: Vec<u8> = get_compressed_cacher_info(&options, &data);
+    let decompressed_data: CacherReturnInfo = get_decompressed_data(&compressed_info);
+
+    assert_eq!(data, decompressed_data.data());
 }
