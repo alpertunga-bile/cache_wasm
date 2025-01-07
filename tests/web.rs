34cdb50ef555f81d74e3c315730ee2ac56392e75
alpertunga-bile <kingdom19online@hotmail.com>
2025-01-07 21:15:41 +0300

    trigger action workflow

--- tests/web.rs
+++ tests/web.rs
@@ -15,7 +15,7 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 fn hello_world_test() {
     let options = CacherOptions::new();
-    let data = String::from("Hello World");
+    let data = String::from("Hello World!");
 
     let compressed_info: Vec<u8> = get_compressed_cacher_info(&options, &data);
     let decompressed_data: CacherReturnInfo = get_decompressed_data(&compressed_info);
